f4c32b065d5ee688d05a240aea6ce38bc01f065a
Andy Theuninck <andy@gohanman.com>
2016-12-05 16:12:59 -0600

    Day 3 pt 2

--- Day03/Program.fs
+++ Day03/Program.fs
@@ -5,10 +5,10 @@ let valid t =
     (t.sideA+t.sideB > t.sideC) && (t.sideA+t.sideC > t.sideB) && (t.sideB+t.sideC > t.sideA)
 
 let matcher = new System.Text.RegularExpressions.Regex("\s*([0-9]+)\s+([0-9]+)\s+([0-9]+)\s*")
+let toInt (s:string) = System.Int32.Parse(s)
 
 let tryTriangle (s:string) =
     let m = matcher.Match(s)
-    let toInt (s:string) = System.Int32.Parse(s)
     match m.Success with
     | false -> None
     | true -> (
@@ -19,6 +19,16 @@ let tryTriangle (s:string) =
                 Some { sideA=a; sideB=b; sideC=c }
     )
 
+let tryTrio (s:string list) =
+    let regex = List.map (fun i -> matcher.Match(i)) s
+    seq {
+        for i in 1..3 do
+            let a = toInt (regex.Item(0).Groups.Item(i).Value)
+            let b = toInt (regex.Item(1).Groups.Item(i).Value)
+            let c = toInt (regex.Item(2).Groups.Item(i).Value)
+            yield { sideA=a; sideB=b; sideC=c }
+    }
+
 [<EntryPoint>]
 let main argv = 
     let file = System.IO.File.ReadAllLines("Input.txt") |> Array.toList
@@ -28,4 +38,13 @@ let main argv =
         |> List.choose id
         |> List.filter valid
     printfn "Triangles: %d" triangles.Length
+
+    let trios = List.chunkBySize 3 file
+    let verts =
+        trios
+        |> List.map tryTrio
+        |> List.map Seq.toList
+        |> List.fold (fun c i -> c @ i) []
+        |> List.filter valid
+    printfn "Vertical Triangles: %d" verts.Length
     0 // return an integer exit code
