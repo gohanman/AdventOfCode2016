83f11e665b48214a6cbc587e704f710e027303e6
Andy Theuninck <andy@gohanman.com>
2016-12-06 13:27:06 -0600

    Day 5 pt 2

--- Day05/Program.fs
+++ Day05/Program.fs
@@ -14,13 +14,34 @@ let rec nextDigit (doorID:string) (index:int) =
     | true -> (hashString.Substring(5, 1), index+1)
     | false -> nextDigit doorID (index+1)
 
+let secondGen (doorID:string) (index:int) (map:Map<int,string>) =
+    let hashString = getHash (doorID + (index.ToString()))
+    let parsed,pos = System.Int32.TryParse(hashString.Substring(5, 1))
+    let value = hashString.Substring(6, 1)
+    printfn "%d %s %s" pos value hashString
+    if (parsed && (pos > 7 || map.ContainsKey(pos))) then
+        map
+    elif not(parsed) then
+        map
+    else
+        let newMap = map.Add(pos, value)
+        newMap
+
 [<EntryPoint>]
 let main argv = 
     let mutable i = 0
-    [0..7]
-    |> List.iter (fun j ->
+    let mutable charMap:Map<int,string> = Map.empty
+    let mutable searching = true
+    printfn "%s" (System.Int32.TryParse("A").ToString())
+    while searching do
         let digit, nextIndex = nextDigit "ojvtpuvg" i
-        printfn "%s" digit
+        let newMap = secondGen "ojvtpuvg" (nextIndex-1) charMap
+        charMap <- newMap
         i <- nextIndex
-    )
+        printfn "First Gen: %s" digit
+        if (charMap.Count >= 8) then
+            printfn "Second Gen: %A" (Map.toList charMap)
+            searching <- false
+        else
+            printfn "Parial Second: %A" (Map.toList charMap)
     0 // return an integer exit code
