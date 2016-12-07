51728fb9e8a649fd476c4abfe2c8150b4f38d35e
Andy Theuninck <andy@gohanman.com>
2016-12-07 11:09:51 -0600

    Day 7 pt 2

--- Day07/Program.fs
+++ Day07/Program.fs
@@ -26,11 +26,7 @@ let rec parse (ip:IPv7) (remaining:string list) =
             parse newIP newRemain
 
 let isABBA (s:string) (i:int) =
-    let a = s.Substring(i, 1)
-    let b = s.Substring(i+1, 1)
-    let c = s.Substring(i+2, 1)
-    let d = s.Substring(i+3, 1)
-    (a<>b && b=c && a=d)
+    (s.[i]<>s.[i+1] && s.[i+1]=s.[i+2] && s.[i]=s.[i+3])
 
 let rec findABBA (s:string) (i:int) =
     match ((i+3) < s.Length) with
@@ -40,6 +36,33 @@ let rec findABBA (s:string) (i:int) =
         | true -> true
         | false -> findABBA s (i+1)
 
+let isABA (s:string) (i:int) =
+    (s.[i]<>s.[i+1] && s.[i]=s.[i+2])
+
+let findABAs (s:string) =
+    seq {
+        for i in 0..(s.Length-3) do
+            if (isABA s i) then yield (s.Substring(i, 3))
+    }
+
+let rec allABAs (sl:string list) (ret:string list) =
+    match sl.Length with
+    | 0 -> ret
+    | _ ->
+        let ABAs = findABAs sl.Head |> Seq.toList
+        allABAs sl.Tail (List.append ret ABAs)
+
+let isReverse (a:string) (b:string) =
+    match (a.Length = 3 && b.Length =3) with
+    | false -> false
+    | true ->
+        ((isABA a 0) && (isABA b 0) && a.[0]=b.[1] && a.[1]=b.[0])
+
+let hasReverse (s:string) (opts:string list) =
+    opts
+    |> List.map (fun i -> isReverse i s)
+    |> List.fold (fun c i -> c || i) false
+
 let validIP (ip:IPv7) =
     let entryABBA = 
         List.map (fun i -> findABBA i 0) ip.entries
@@ -48,6 +71,12 @@ let validIP (ip:IPv7) =
         List.map (fun i -> findABBA i 0) ip.hypernets
         |> List.fold (fun c i -> c || i) false
     (entryABBA && not(hyperABBA))
+
+let validSSL (ip:IPv7) =
+    let entryABA = allABAs ip.entries []
+    let hyperABA = allABAs ip.hypernets []
+    let reverses = List.map (fun i -> hasReverse i hyperABA) entryABA
+    List.fold (fun c i -> c || i) false reverses
              
 [<EntryPoint>]
 let main argv = 
@@ -55,4 +84,6 @@ let main argv =
     let ips = List.map (fun i -> parse emptyIP [i]) file
     let validIPs = List.filter validIP ips
     printfn "Valid IPs: %d" (validIPs.Length)
+    let sslIPs = List.filter validSSL ips
+    printfn "Valid IPs: %d" (sslIPs.Length)
     0 // return an integer exit code
