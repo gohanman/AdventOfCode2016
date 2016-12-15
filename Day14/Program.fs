de2d1450939074072181e83ec1003480fafda118
Andy Theuninck <andy@gohanman.com>
2016-12-15 17:15:28 -0600

    14

---
+++ Day14/Program.fs
@@ -0,0 +1,59 @@
+﻿
+[<Literal>]
+let SALT = "zpqevtbw"
+
+let repeatRegex (n:int) =
+    let nums = [0..9] |> List.map (fun i -> i.ToString())
+    let chars = ['a'..'f'] |> List.map (fun i -> i.ToString())
+    let sb = new System.Text.StringBuilder()
+    for c in (nums @ chars) do
+        sb.Append(c.PadLeft(n, c.[0])) |> ignore
+        sb.Append("|") |> ignore
+    "(" + sb.ToString().TrimEnd([| '|' |]) + ")"
+
+let TRIO = new System.Text.RegularExpressions.Regex(repeatRegex 3)
+
+let md5 (s:string) =
+    let bytes = System.Text.Encoding.UTF8.GetBytes(s)
+    let md5 = System.Security.Cryptography.MD5.Create()
+    let result = md5.ComputeHash(bytes)
+    System.BitConverter.ToString(result).Replace("-", "").ToLower()
+
+let stretchedMD5 (s:string) =
+    let mutable result = s
+    for i in [0..2015] do
+        result <- md5 s
+    result
+
+let rec hasSubKey (hasher:string->string) (index:int) (seq:string) (iter:int) =
+    match iter with
+    | 1000 -> false
+    | _ ->
+        let sub = hasher (SALT + index.ToString())
+        match (sub.Contains(seq)) with
+        | true -> 
+            true
+        | false -> hasSubKey hasher (index+1) seq (iter+1)
+
+let isKey hasher (index:int) =
+    let hashed = hasher (SALT + index.ToString())
+    let t = TRIO.Match(hashed)
+    match t.Success with
+    | true ->
+        let str = t.Groups.Item(1).Value
+        let seq = "".PadLeft(5, str.[0])
+        if (hasSubKey (hasher) (index+1) seq 0) then Some hashed else None
+    | false -> None
+
+let rec findKeys hasher (index:int) (keys:string list) (num:int) =
+    if (num=keys.Length) then List.rev keys
+    else
+        match (isKey (hasher) index) with
+        | Some k -> findKeys hasher (index+1) ((index.ToString()+":"+k) :: keys) num
+        | None -> findKeys hasher (index+1) keys num
+
+[<EntryPoint>]
+let main argv = 
+    let keys = findKeys (stretchedMD5) 0 [] 64
+    keys |> List.iteri (fun i x -> printfn "%d %s" i x)
+    0 // return an integer exit code
---
+++ Day15/Program.fs
@@ -0,0 +1,53 @@
+﻿
+type Disc = { positions:int; current: int }
+type World = { discs:Disc array; time:int; capsule:int }
+
+let rotateDisc (d:Disc) =
+    let pos = (d.current + 1) % d.positions
+    { d with current=pos }
+
+let capsuleFalling (w:World) =
+    let index = w.capsule - 1
+    match (index >= 0 && index < w.discs.Length) with
+    | false -> true
+    | true -> if (w.discs.[index].current=0) then true else false
+
+let finished (w:World) = w.capsule > w.discs.Length
+
+let tick (w:World) =
+    let d' = w.discs |> Array.map (fun i -> rotateDisc i)
+    { discs=d'; time=(w.time+1); capsule=(w.capsule+1) }
+
+let rec tickN (w:World) (n:int) =
+    if (n=0) then w
+    else tickN (tick w) (n-1)
+
+let rec safePassage (w:World) =
+    if (finished w) then true
+    elif (not (capsuleFalling w)) then false
+    else safePassage (tick w)
+
+let rec tryTimes (d:Disc array) (t:int) =
+    let w = { discs=d; time=t; capsule=0 }
+    let w' = { (tickN w t) with capsule=0 }
+    if (safePassage w') then t
+    else tryTimes d (t+1)
+
+[<EntryPoint>]
+let main argv = 
+    let d = [|
+        { positions=17; current=1 };
+        { positions=7; current=0 };
+        { positions=19; current=2 };
+        { positions=5; current=0 };
+        { positions=3; current=0 };
+        { positions=13; current=5 };
+    |]
+    (*
+    let d = [|
+        { positions=5; current=4 };
+        { positions=2; current=1 };
+    |]
+    *)
+    printfn "%d" (tryTimes d 0)
+    0 // return an integer exit code
