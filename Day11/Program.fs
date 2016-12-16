d832418c22b192da5bf1d8fce0355ee3e857e7f3
Andy Theuninck <andy@gohanman.com>
2016-12-15 22:24:53 -0600

    Updated day 11

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -5,60 +5,49 @@ type Element =
     | Thulium = 69
     | Plutonium = 94
     | Curium = 96
+    | Elerium = 100
+    | Dilithium = 101
 
 type Part =
     | Generator of Element
     | Microchip of Element
 
-type Floor = { parts:Part list }
+type Floor = { parts:Set<Part> }
 type World = { floors:Floor array; elevator:int }
-type Move = { parts:Part list; source:int; dest:int; }
+type Move = { parts:Set<Part>; source:int; dest:int; }
+type MetaFloor = { chips:int; generators:int; pairs:int }
+type MetaWorld = { floors:MetaFloor array; elevator:int }
+
+let isGenerator (p:Part) =
+    match p with
+    | Generator g -> true
+    | _ -> false
 
 let safeOnFloor (p:Part) (f:Floor) =
     match p with
-    | Generator g ->
-        let g' = int g
-        f.parts
-        |> List.filter (fun i ->
-            match i with
-            | Microchip m -> (int m)=g'
-            | _ -> false
-        )
-        |> List.isEmpty |> not
+    | Generator g -> true
     | Microchip m ->
         let m' = int m
-        f.parts
-        |> List.filter (fun i ->
-            match i with
-            | Generator g -> (int g)=m'
-            | _ -> false
-        )
-        |> List.isEmpty |> not
+        let generators = f.parts |> Seq.filter isGenerator |> Seq.toList
+        if (generators.Length=0) then true
+        else
+            f.parts
+            |> Set.filter (fun i ->
+                match i with
+                | Generator g -> (int g)=m'
+                | _ -> false
+            )
+            |> Set.isEmpty |> not
 
 let rankWorld (w:World) =
     w.floors
-    |> Array.mapi (fun i x -> i * x.parts.Length)
+    |> Array.mapi (fun i x -> i * x.parts.Count)
     |> Array.fold (fun c i -> c + i) 0
 
-let addParts (w:World) (floor:int) (p:Part list) =
-    let a =
-        w.floors
-        |> Array.mapi (fun i x ->
-            if (i=floor) then { x with parts=(p @ x.parts) }
-            else x
-        )
+let addParts (w:World) (floor:int) (p:Set<Part>) =
+    let a = Array.copy w.floors
+    a.[floor] <- { parts=(Set.union p w.floors.[floor].parts) }
     { w with floors=a }
-
-let partEquals (a:Part) (b:Part) =
-    match a with
-    | Generator g ->
-        match b with
-        | Generator g' -> (int g)=(int g')
-        | _ ->  false
-    | Microchip m ->
-        match b with
-        | Microchip m' -> (int m)=(int m')
-        | _ -> false
 
 let partMatches (a:Part) (b:Part) =
     match a with
@@ -71,41 +60,31 @@ let partMatches (a:Part) (b:Part) =
         | Generator g -> (int m)=(int g)
         | Microchip m -> false
 
-let floorMatches (a:Floor) (b:Floor) =
-    let f =
-        a.parts
-        |> List.map (fun i -> 
-            List.exists (fun j -> partEquals i j) b.parts
-        )
-        |> List.fold (fun c i -> c && i) true
-    (f && a.parts.Length=b.parts.Length)
+let metaFloor (f:Floor) =
+    let g = f.parts |> Set.filter isGenerator |> Set.count
+    let m = f.parts.Count - g
+    let arr = Set.toArray f.parts
+    let mutable p = 0
+    for i in [0..(arr.Length - 1)] do
+        for j in [(i+1)..(arr.Length - 1)] do
+            if (partMatches arr.[i] arr.[j]) then p <- p + 1
+    { chips=m; generators=g; pairs=p; }
 
-let worldMatches (w:World) (x:World) =
-    let f =
-        w.floors
-        |> Array.mapi (fun i j ->
-            floorMatches j (x.floors.[i])
-        )
-        |> Array.fold (fun c i -> c && i) true
-    (f && w.elevator=x.elevator)
+let metaState (w:World) =
+    let f = Array.copy w.floors
+    let mf = Array.map (fun i -> metaFloor i) f
+    { MetaWorld.floors=mf; elevator=w.elevator }
 
 let worldChecked (wl:World seq) (w:World) =
     Seq.contains w wl
-    //Seq.exists (fun i -> worldMatches i w) wl
     
-let rec removeParts (w:World) (floor:int) (p:Part list) =
-    match p.Length with
-    | 0 -> w
-    | _ ->
-        let cur = p.Head
-        let a = 
-            w.floors
-            |> Array.mapi (fun i x ->
-                if (i=floor) then { Floor.parts=(List.filter (fun j -> not(partEquals cur j)) x.parts) }
-                else x
-            )
-        let w' = { w with floors=a }
-        removeParts w' floor p.Tail
+let removeParts (w:World) (floor:int) (p:Set<Part>) =
+    let keep,_ = 
+        w.floors.[floor].parts 
+        |> Set.partition (fun i -> not (p.Contains(i)))
+    let f = Array.copy w.floors
+    f.[floor] <- { parts=keep }
+    { w with floors=f }
 
 let solved (w:World) =
     let top = w.floors.Length - 1
@@ -119,86 +98,106 @@ let solved (w:World) =
 let canMoveUp (w:World) = w.elevator < (w.floors.Length - 1)
 let canMoveDown (w:World) = w.elevator > 0
 
-let rec moves (w:World) =
+let moves (w:World) =
     seq {
         let f = w.floors.[w.elevator]
+        let pl = Set.toArray f.parts
         let source=w.elevator
         if (canMoveUp w) then
             let dest = w.elevator+1
             let up = w.floors.[dest]
-            for i in f.parts do
+            for index in [0..(pl.Length - 1)] do
+                let i = pl.[index]
                 if (safeOnFloor i up) then 
-                    yield { parts=[i]; source=source; dest=dest }
-                for j in f.parts.Tail do
+                    yield { parts=Set.empty.Add(i); source=source; dest=dest }
+                for jndex in [(index+1)..(pl.Length - 1)] do
+                    let j = pl.[jndex]
                     if ((safeOnFloor i up) && (safeOnFloor j up)) then
-                        yield { parts=[i;j]; source=source; dest=dest }
+                        yield { parts=Set.empty.Add(i).Add(j); source=source; dest=dest }
                     elif (partMatches i j) then
-                        yield { parts=[i;j]; source=source; dest=dest }
+                        yield { parts=Set.empty.Add(i).Add(j); source=source; dest=dest }
         if (canMoveDown w) then
             let dest = w.elevator-1
             let down = w.floors.[dest]
-            for i in f.parts do
+            for index in [0..(pl.Length - 1)] do
+                let i = pl.[index]
                 if (safeOnFloor i down) then 
-                    yield { parts=[i]; source=source; dest=dest }
-                for j in f.parts.Tail do
-                    if ((safeOnFloor i down) && (safeOnFloor j down)) then
-                        yield { parts=[i;j]; source=source; dest=dest }
-                    elif (partMatches i j) then
-                        yield { parts=[i;j]; source=source; dest=dest }
-        if (f.parts.Length > 1) then
-            let f' =
-                w.floors            
-                |> Array.mapi (fun i x ->
-                    if (i=source) then { Floor.parts=f.parts.Tail }
-                    else x
-                )
-            yield! moves { w with floors=f' }
+                    yield { parts=Set.empty.Add(i); source=source; dest=dest }
+                else
+                    for jndex in [(index+1)..(pl.Length - 1)] do
+                        let j = pl.[jndex]
+                        if ((safeOnFloor i down) && (safeOnFloor j down)) then
+                            yield { parts=Set.empty.Add(i).Add(j); source=source; dest=dest }
+                        elif (partMatches i j) then
+                            yield { parts=Set.empty.Add(i).Add(j); source=source; dest=dest }
     }
+
+let itemCount (w:World) =
+    w.floors |> Array.fold (fun c i -> c + i.parts.Count) 0
 
 let makeMove (w:World) (m:Move) =
     let w' = removeParts w m.source m.parts
     let w'' = addParts w' m.dest m.parts
-    let dir = if m.source < m.dest then 1 else -1
-    { w'' with elevator=(w.elevator + dir) }
+    if ((itemCount w')=(itemCount w'')) then 
+        printfn ""
+        printfn "MOVE %A" m
+        printfn "PRE %A" w'
+        printfn "POST %A" w''
+        printfn ""
+    { w'' with elevator=m.dest }
 
-let rec solve (w:World seq) (num:int) (visited:World seq) =
-    if (Seq.exists (fun i -> solved i) w) then
-        printfn "Solved in %d" num
-    elif (num < 1024) then
-        let visits = Seq.append visited w
-        let w' =
-            w
-            |> Seq.map (fun i -> 
-                let m = moves i
-                Seq.map (fun j -> makeMove i j) m
-            )
-            |> Seq.concat
-            |> Seq.filter (fun i -> not (worldChecked visits i))
-        printfn "%d" num
-        w'
-        |> Seq.iter (fun i ->
-            solve [i] (num+1) (Seq.append w' visits)
-        )
+let floorCount (w:World) (n:int) =
+    w.floors.[n].parts.Count
+
+let solve (w:World) =
+    let mutable searching = true
+    let mutable count = 0
+    let mutable visited = Set.empty.Add(metaState w)
+    let mutable queue = Set.empty.Add(w)
+    while (searching) do
+        printfn "%d, %d" count visited.Count
+        for world in queue do
+            if (solved world) then  
+                printfn "Solved in %d" count
+                searching <- false
+            else
+                let m = moves world
+                let worlds = 
+                    m 
+                    |> Seq.map (fun i -> makeMove world i) 
+                    |> Seq.filter (fun i -> not (visited.Contains(metaState i)))
+                    |> Seq.sortBy rankWorld
+                    |> Seq.toList
+                for newWorld in worlds do
+                    queue <- queue.Add(newWorld)
+                    visited <- visited.Add(metaState newWorld)
+        count <- count + 1
 
 [<EntryPoint>]
 let main argv = 
     let floors = [|
-        { Floor.parts=[ 
-                        Generator Element.Strontium; 
-                        Microchip Element.Strontium;
-                        Generator Element.Plutonium; 
-                        Microchip Element.Plutonium;
-        ] };
-        { parts=[ 
-                Generator Element.Thulium;
-                Generator Element.Ruthenium;
-                Microchip Element.Ruthenium;
-                Generator Element.Curium;
-                Microchip Element.Curium;
-        ] };
-        { parts=[ Microchip Element.Thulium] };
-        { parts=[] };
+        { Floor.parts=Set.empty
+            .Add(Generator Element.Strontium) 
+            .Add(Microchip Element.Strontium)
+            .Add(Generator Element.Plutonium)
+            .Add(Microchip Element.Plutonium)
+        };
+        { parts=Set.empty
+            .Add(Generator Element.Thulium)
+            .Add(Generator Element.Ruthenium)
+            .Add(Microchip Element.Ruthenium)
+            .Add(Generator Element.Curium)
+            .Add(Microchip Element.Curium)
+        };
+        { parts=Set.empty.Add(Microchip Element.Thulium) };
+        { parts=Set.empty};
     |]
-    let world = { floors=floors; elevator=0 }
-    solve [world] 0 []
+    let world = { World.floors=floors; elevator=0 }
+    //solve world
+    let f0 = world.floors.[0].parts
+    let additions = 
+        f0.Add(Generator Element.Elerium).Add(Microchip Element.Elerium).Add(Generator Element.Dilithium).Add(Microchip Element.Dilithium)
+    floors.[0] <- { parts=additions }
+    let world' = { World.floors=floors; elevator=0 }
+    solve world'
     0 // return an integer exit code
